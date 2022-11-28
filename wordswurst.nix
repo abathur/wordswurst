d3e687a29751d3a087c21ff751746feeb9164711
Travis A. Everett <travis.a.everett@gmail.com>
2022-11-27 22:37:43 -0600

    keep old cssselect2 for now

--- wordswurst.nix
+++ wordswurst.nix
@@ -17,6 +17,29 @@ let
     version = "unstable";
   };
 
+  /* TODO: temporarily extracting old version from nixpkgs; ww needs
+  to adapt to something about the 5.x or 6.x series but time's short */
+  cssselect2 = with python39.pkgs; buildPythonPackage rec {
+    pname = "cssselect2";
+    version = "0.4.1";
+    disabled = pythonOlder "3.5";
+
+    src = fetchPypi {
+      inherit pname version;
+      sha256 = "93fbb9af860e95dd40bf18c3b2b6ed99189a07c0f29ba76f9c5be71344664ec8";
+    };
+
+    postPatch = ''
+      sed -i '/^addopts/d' pyproject.toml
+    '';
+
+    propagatedBuildInputs = [ tinycss2 ];
+
+    checkInputs = [ pytestCheckHook ];
+
+    pythonImportsCheck = [ "cssselect2" ];
+  };
+
 in
 python39.pkgs.buildPythonPackage {
   name = "wordswurst-test";
@@ -25,7 +48,7 @@ python39.pkgs.buildPythonPackage {
   # dontInstall = true;
   propagatedBuildInputs = [
     python39.pkgs.tinycss2
-    python39.pkgs.cssselect2
+    cssselect2
     python39.pkgs.inflect
     dmark
   ];
